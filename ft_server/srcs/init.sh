fd014d6ef261546c1f7079228a71ad7f5e2531ca
Yoko Zena <yzena@am-j2.msk.21-school.ru>
2021-02-23 15:21:12 +0300

    server

---
+++ ft_server/srcs/init.sh
@@ -0,0 +1,15 @@
+service nginx start
+service mysql start
+service php7.3-fpm start
+
+# Configure a wordpress database
+echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
+echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
+
+bash
+
+rm -rf /etc/nginx/sites-enabled/default
+rm -rf /etc/nginx/sites-available/default
+sleep infinity
